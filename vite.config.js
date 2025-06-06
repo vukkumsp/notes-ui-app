1bcd1ac54f907d1cc0460dc466e4fde2bcf7d909
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-06 23:32:05 +0530

    configured, updated scripts to deploy to gh-pages seemlessly.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: '/notes-ui-app/', //this is required for deployment on GitHub Pages without a custom domain
   plugins: [react()],
 })
