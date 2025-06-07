1acfe19bac9e8d497f54b59ab3350484cbc0c86c
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-07 09:13:59 +0530

    connected to backend service;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import './App.css'
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import { useEffect, useState } from 'react';
+import { TEST_API_URL } from './services/rest/RESTAPI';
 
 function App() {
+  const [test, setTest] = useState('');
+
+  useEffect(() => {
+  fetch(TEST_API_URL)
+    .then(response => {
+      
+      if (!response.ok) throw new Error('Network error');
+      
+      return response.text();
+    })
+    .then(response => {
+      setTest(response)
+      console.log(response);
+    })
+    .catch(error => console.error(error));
+}, []);
 
   return (
     <>
       <Header />
       <div className="container">
         <h2>Welcome to the Notes App</h2>
+        <h3>Response : {test?test: 'Loading...'}</h3>
         <p>This is a simple application to manage your notes.</p>
         <p>Feel free to add, edit, and delete notes as you wish.</p>
         
---
+++ src/services/rest/RESTAPI.jsx
@@ -0,0 +1,13 @@
+/**
+ * REST API URL constants for the Notes UI App
+ */
+
+export const BASE_URL = 'https://notes-service-iwwy.onrender.com';
+export const API_BASE_URL = `${BASE_URL}/api`;
+
+export const TEST_API_URL = `${API_BASE_URL}/test`;
+// export const NOTES_API_URL = `${API_BASE_URL}/notes`;
+// export const USERS_API_URL = `${API_BASE_URL}/users`;
+// export const AUTH_API_URL = `${API_BASE_URL}/auth`;
+
+// Add more endpoints as needed
