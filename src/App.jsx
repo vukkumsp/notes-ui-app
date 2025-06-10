8af6ce06ddf1aeb97c8ff9ee8d3ae04b4820dfa5
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-10 20:47:20 +0530

    code cleanup;

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import './App.css'
-import Header from './components/header/Header';
-import { useEffect, useState } from 'react';
-import { TEST_API_URL, NOTES_API_URL } from './services/rest/RESTAPI';
-import Footer from './components/footer/Footer';
-import Note from './components/note/Note';
-import { getNotes } from './services/rest/noteService';
-import { getGuestToken } from './services/rest/authService';
-import { getCookie, setCookie } from './services/cookieManagement';
 import HomeView from './views/home-view/HomeView';
 
 function App() {
