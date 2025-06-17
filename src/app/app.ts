1a0903aca9708c0210dc979fc25e115e9bf36e2e
V. Sai Prakash <vukkumsp@gmail.com>
2025-06-17 14:59:04 +0530

    code cleanup;

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { environment } from './environment';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
